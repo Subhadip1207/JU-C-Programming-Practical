0923dc1e290d4ccb78017750376163b7ea2146a8
Subhadip1207 <subhadip003.dutta@gmail.com>
2024-11-19 19:39:19 +0530

    Enter newline operator

--- SizeOfDataTypes.c
+++ SizeOfDataTypes.c
@@ -2,9 +2,9 @@
 datatypes.*/
 #include<stdio.h>
 int main(){
-    printf("The size of char is=%d",sizeof(char));
-    printf("The size of integer is=%d",sizeof(int));
-    printf("The size of float is=%d",sizeof(float));
-    printf("The size of double is=%d",sizeof(double));
+    printf("The size of char is=%d\n",sizeof(char));
+    printf("The size of integer is=%d\n",sizeof(int));
+    printf("The size of float is=%d\n",sizeof(float));
+    printf("The size of double is=%d\n",sizeof(double));
     return 0;
 }
