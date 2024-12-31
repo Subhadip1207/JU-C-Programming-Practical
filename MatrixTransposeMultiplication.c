04e32eed6ee24bc1a12a8f0bde747e1b51dbf835
Subhadip1207 <subhadip003.dutta@gmail.com>
2024-12-31 15:46:42 +0530

    MUltiplication of a matrix with its transpose

--- MatrixTransposeMultiplication.c
+++ MatrixTransposeMultiplication.c
@@ -1,13 +1,13 @@
 /*Write a program to take one 2-D array, find the transpose of  it, finally print the multiplied value of the matrix with its transpose.*/
 #include<stdio.h>
 int main(){
-    int row,column,i,j;
+    int row,column,i,j,k;
    
     printf("Enter the rows of the matrix : ");
     scanf("%d",&row);
     printf("Enter the columns of the matrix : ");
     scanf("%d",&column);
-    int a[row][column],b[column][row];
+    int a[row][column],b[column][row],c[row][row];
     printf("Enter the elements in the matrix ...\n");
     for(i=0;i<row;i++){
         for(j=0;j<column;j++){
@@ -23,6 +23,21 @@ int main(){
     for(i=0;i<column;i++){
         for(j=0;j<row;j++){
             printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<row;i++){
+        for(j=0;j<row;j++){
+            c[i][j]=0;
+            for(k=0;k<column;k++){
+                c[i][j] += a[i][k]*b[k][j];
+            }
+        }
+    }
+    printf("Matrix Multiplication is : \n");
+    for(i=0;i<row;i++){
+        for(j=0;j<row;j++){
+            printf("%d\t",c[i][j]);
         }
         printf("\n");
     }
